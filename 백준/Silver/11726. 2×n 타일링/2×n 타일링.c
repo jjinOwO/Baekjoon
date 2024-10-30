1bef29c8172cda192ba3f85bb948768a510de969
김호진 <91896294+jjinOwO@users.noreply.github.com>
2024-10-30 18:32:43 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+    int count = 0;
+    scanf("%d",&n);
+    int dp[n];
+    dp[0] = 1;
+    dp[1] = 2;
+    for(int i = 2; i < n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2])%10007;
+    }
+    printf("%d",dp[n-1]);
+    return 0;
+}
